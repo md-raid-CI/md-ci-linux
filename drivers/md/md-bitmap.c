ab882cd889cdab9a2ff42d7db07a5d24b294251f
Yu Kuai <yukuai3@huawei.com>
2024-10-24 21:13:23 +0800

    md/md-bitmap: merge md_bitmap_group into bitmap_operations
    Now that all bitmap implementations are internal, it doesn't make sense
    to export md_bitmap_group anymore.
    
    Signed-off-by: Yu Kuai <yukuai3@huawei.com>

--- drivers/md/md-bitmap.c
+++ drivers/md/md-bitmap.c
@@ -2963,7 +2963,8 @@ static struct attribute *md_bitmap_attrs[] = {
 	&max_backlog_used.attr,
 	NULL
 };
-const struct attribute_group md_bitmap_group = {
+
+static struct attribute_group md_bitmap_group = {
 	.name = "bitmap",
 	.attrs = md_bitmap_attrs,
 };
@@ -2996,6 +2997,8 @@ static struct bitmap_operations bitmap_ops = {
 	.copy_from_slot		= bitmap_copy_from_slot,
 	.set_pages		= bitmap_set_pages,
 	.free			= md_bitmap_free,
+
+	.group			= &md_bitmap_group,
 };
 
 void mddev_set_bitmap_ops(struct mddev *mddev)
